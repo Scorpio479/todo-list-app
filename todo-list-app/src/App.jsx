db20941f96373810db6af330fde0420b98e21095
Hadi Koleilat <koleilat@hotmail.com>
2025-09-29 19:33:02 -0500

    Update App.jsx

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "./components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
